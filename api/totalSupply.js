c7435a51ae43d2df1937ff8c5c90d4847b90b2f1
0xCanet <0xcanet@gmail.com>
2025-06-24 17:01:34 +0200

    totalSupply back to ethers5

--- api/totalSupply.js
+++ api/totalSupply.js
@@ -1,6 +1,7 @@
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
-const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+
 const contractAddress = process.env.CONTRACT_ADDRESS;
 
 const abi = ["function totalSupply() view returns (uint256)"];
