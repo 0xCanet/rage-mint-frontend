f3f7b53aecfacb687ec55a0c416edf16754e25db
0xCanet <0xcanet@gmail.com>
2025-06-24 19:20:05 +0200

    fix: add eslint-config-next

--- pages/main.jsx
+++ pages/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './index.jsx'; // adapte le chemin
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
